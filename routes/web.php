48a9bea0fb09403f8ad93a3173beaff8e9868ac8
Tosca Yoel Connery <toscayoel.connery@webimp.com.sg>
2019-04-20 23:14:25 +0700

    fixing footer to the bottom

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@
 
 Route::get('/', 'SiteController@get_index');
 Route::get('/index', 'SiteController@get_index');
+Route::get('/home', 'SiteController@get_index');
 
-Route::get('/product/{jenis_produk?}', 'ProductController@get_product');
+Route::get('/produk/{jenis_produk?}', 'ProductController@get_product');
 
 Route::get('/contact', 'SiteController@get_contact');
 
@@ -22,16 +23,16 @@ Route::get('/blog-list', 'BlogController@get_blog_list');
 
 Route::get('/blog/{id}', 'BlogController@get_blog');
 
-Route::post('/berikan-komentar-blog', 'BlogController@berikan_komentar_blog');
+Route::post('/berikan_komentar_blog', 'BlogController@berikan_komentar_blog');
 
-Route::get('/tambahkan-blog', 'BlogController@tambahkan_blog');
+Route::get('/tambahkan_blog', 'BlogController@tambahkan_blog');
 
 Route::get('/history', 'SiteController@get_history');
 
-Route::get('/vision-mission', 'SiteController@get_vision_mission');
+Route::get('/visi_misi', 'SiteController@get_vision_mission');
 
 Route::get('/founder', 'SiteController@get_founder');
 
-Route::post('/kritik-saran', 'KritikSaranController@post_kritik_saran');
+Route::post('/kritik_saran', 'KritikSaranController@post_kritik_saran');
 
-Route::get('/lihat-kritik-saran', 'KritikSaranController@get_kritik_saran');
+Route::get('/lihat_kritik_saran', 'KritikSaranController@get_kritik_saran');
